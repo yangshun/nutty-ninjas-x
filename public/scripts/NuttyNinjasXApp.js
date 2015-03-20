3d1e52c54bccb0c9509cbf8f8bf9ce682e3682ec
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-03-21 01:24:32 +0800

    Remove unused files

--- public/scripts/NuttyNinjasXApp.js
+++
@@ -1,7 +0,0 @@
-define('NuttyNinjasXApp', ['ScoreBoardController'],
-  function (ScoreBoardController) {
-  
-    var app = angular.module('NuttyNinjasX', []);
-    app.controller('ScoreBoardController', ScoreBoardController);
-  
-  });
