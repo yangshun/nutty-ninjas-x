5322343f62fe422f960873d67cd390e117528498
Nguyen Trung Hieu <ngtrhieu0011@gmail.com>
2015-04-17 20:43:03 +0800

    Revert "Fix bug in include ordering"
    This reverts commit 94ccca98c3f51371836767bc3478beaf5a17778a.

--- public/scripts/game/Init.js
+++ public/scripts/game/Init.js
@@ -13,26 +13,3 @@ Q.SPRITE_PLAYER = 1;
 Q.SPRITE_COLLECTABLE = 2;
 Q.SPRITE_DOOR = 8;
 Q.SPRITE_UI = 32;
-
-Q.loadTMX("level3.tmx, collectables.json, doors.json, enemies.json, fire.mp3, jump.mp3, heart.mp3, hit.mp3, coin.mp3, player.json, player.png, shuriken.png, whirlpool.png, shurikenRed.png, whirlpool-pink.png, whirlpool-blue.png", function() {
-  Q.compileSheets("player.png","player.json");
-  Q.compileSheets("collectables.png","collectables.json");
-  Q.compileSheets("enemies.png","enemies.json");
-  Q.compileSheets("doors.png","doors.json");
-  Q.animations("player", {
-    walk_right: { frames: [0,1,2,3,4,5,6,7,8,9,10], rate: 1/15, flip: false, loop: true },
-    walk_left: { frames:  [0,1,2,3,4,5,6,7,8,9,10], rate: 1/15, flip:"x", loop: true },
-    jump_right: { frames: [13], rate: 1/10, flip: false },
-    jump_left: { frames:  [13], rate: 1/10, flip: "x" },
-    stand_right: { frames:[14], rate: 1/10, flip: false },
-    stand_left: { frames: [14], rate: 1/10, flip:"x" },
-    duck_right: { frames: [15], rate: 1/10, flip: false },
-    duck_left: { frames:  [15], rate: 1/10, flip: "x" },
-    climb: { frames:  [16, 17], rate: 1/3, flip: false }
-  });
-  Q.stageScene("level3");
-
-  var app = angular.module('NuttyNinjasX', []);
-  app.controller('ScoreBoardController', ScoreBoardController);
-  angular.bootstrap(document, ['NuttyNinjasX']);
-});
