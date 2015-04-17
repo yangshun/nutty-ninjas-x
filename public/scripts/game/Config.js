19da0f5b61e1954efc0a99e18e8169ef7bee31e5
Nguyen Trung Hieu <ngtrhieu0011@gmail.com>
2015-04-17 20:07:05 +0800

    Add Config file

---
+++ public/scripts/game/Config.js
@@ -0,0 +1,30 @@
+var Config = {
+  setup: {
+    development: true,
+    maximize: true,
+    scaleToFit: true
+  },
+  player: {
+    asset: 'player.png',
+    jumpSpeed: -750
+  },
+  bullet: {
+    speed: 450,
+    typeShuriken: 0,
+    typePortal: 1,
+    typeLast: 2
+  },
+  map: {
+    tile: {
+      width: 40,
+      height: 40
+    }
+  },
+  levelName: 'level3.tmx',
+  assets: {
+    tile: {
+      width: 40,
+      height: 40
+    }
+  }
+};
