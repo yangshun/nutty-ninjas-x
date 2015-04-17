779842a6f377b954281ed7093b3c7b19cc3f57d6
Nguyen Trung Hieu <ngtrhieu0011@gmail.com>
2015-04-17 20:23:54 +0800

    Add scene 3 into project

---
+++ public/scripts/scenes/Level3.js
@@ -0,0 +1,58 @@
+Q.scene("level3",function(stage) {
+  Q.stageTMX("level3.tmx",stage);
+  GameState.gameStage = stage;
+  // stage.add("viewport").follow(Q("Player").first());
+
+
+  (function () {
+    function getQueryVariable (variable) {
+      var query = window.location.search.substring(1);
+      var vars = query.split('&');
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (pair[0] === variable) {
+          return pair[1];
+        }
+      }
+      return false;
+    }
+
+    var roomId = getQueryVariable('room');
+    var socket = io(window.location.host + '/game');
+    var names = ['John', 'Mary', 'Jane', 'Peter', 'Bob', 'Karen'];
+    var playerName = getQueryVariable('playerName');
+    if (!playerName) {
+      playerName = names[Math.floor(Math.random() * 6)] + Math.floor(Math.random() * 1000);
+    }
+
+    socket.on('connect', function () {
+      socket.emit('player.join', {
+        name: playerName,
+        roomId: roomId
+      });
+    });
+
+    socket.on('player.connected.self', function (data) {
+      GameState.addPlayer(data, socket);
+    });
+
+    socket.on('player.disconnected', function (data) {
+      GameState.removeActor(data);
+      PubSub.publish('removePlayer', data);
+    });
+
+    socket.on('player.updated', function (data) {
+      GameState.updateActors(data);
+      PubSub.publish('updatePlayer', data);
+    });
+
+    socket.on('player.shoot', function (data) {
+      GameState.actorFire(data);
+    });
+
+    socket.on('connection.rtt.toclient', function () {
+      socket.emit('connection.rtt.fromclient');
+    });
+  })();
+
+});
