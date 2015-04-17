c8d99d56e1242d07f4045444a0040ddb0b4e0b19
Nguyen Trung Hieu <ngtrhieu0011@gmail.com>
2015-04-17 20:08:16 +0800

    Add Init file

---
+++ public/scripts/game/Init.js
@@ -0,0 +1,15 @@
+var Q = window.Q = Quintus({audioSupported: [ 'wav','mp3','ogg' ]})
+        .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI, TMX, Audio")
+        // Maximize this game to whatever the size of the browser is
+        .setup({ maximize: true })
+        // And turn on default input controls and touch input (for UI)
+        .controls(true).touch()
+        // Enable sounds.
+        .enableSound();
+
+// Load and init audio files.
+
+Q.SPRITE_PLAYER = 1;
+Q.SPRITE_COLLECTABLE = 2;
+Q.SPRITE_DOOR = 8;
+Q.SPRITE_UI = 32;
