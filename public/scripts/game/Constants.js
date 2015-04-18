e62952616a1d01ef6473c1b153fd97a7103a728a
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-04-19 02:40:05 +0800

    Use nice hex colours for healthbar

--- public/scripts/game/Constants.js
+++ public/scripts/game/Constants.js
@@ -2,5 +2,11 @@ var Constants = {
   WeaponType: {
     Shuriken: 0,
     Portal: 1,
+  },
+  HexColors: {
+    red: '#fe1b1b',
+    blue: '#1bd6fe',
+    green: '#5bfe1b',
+    yellow: '#fed61b'
   }
 }
